aa8e1e2e758e7bee1f7202a73aeeb5682d355af1
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-29 16:55:06 +0300

    Add task 5

---
+++ HomeWork_lesson4/Task5.TwoDimensionalArray/ArrayTwoDimensional.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.TwoDimensionalArray
+{
+    public class ArrayTwoDimensional
+    {
+        int[,] array;
+        int row;
+        int column;
+        Random random = new Random();
+
+        public int Min
+		{
+            get { return MinElement(); }
+		}
+
+        public int Max
+		{
+            get { return MaxElement(); }
+		}
+
+        public ArrayTwoDimensional(int n, int m)
+		{
+            row = n;
+            column = m;
+            array = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++) array[i, j] = random.Next(-100, 100);
+		}
+
+        public ArrayTwoDimensional(string filename)
+        {
+            if(File.Exists(filename))
+			{
+                string[] arr = File.ReadAllLines(filename);
+                row = arr.Length;
+                column = arr[0].Split().Length;
+                array = new int[row, column];
+
+                for(int i = 0; i < row; i++)
+				{
+                    string[] rowArr = arr[i].Split();
+                    for (int j = 0; j < column; j++) array[i, j] = int.Parse(rowArr[j]);
+				}
+			}
+            else Console.WriteLine("Error load file");
+        }
+
+        private int MinElement()
+		{
+            int min = array[0, 0];
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < column; j++)
+                    if (array[i, j] < min) min = array[i, j];
+
+            return min;
+        }
+
+        private int MaxElement()
+		{
+            int max = array[0, 0];
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < column; j++)
+                    if (array[i, j] > max) max = array[i, j];
+
+            return max;
+        }
+
+        public int SumArray()
+		{
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < column; j++) sum += array[i, j];
+
+            return sum;
+		}
+
+        public int SumArray(int number)
+		{
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < column; j++)
+                    if(array[i,j] > number) sum += array[i, j];
+
+            return sum;
+        }
+
+        public void NumberMaxElement(out int r, out int c)
+		{
+            r = c = 0;
+
+            int max = array[0, 0];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                        r = i + 1;
+                        c = j + 1;
+                    }
+                }
+            }
+        }
+
+        public void ReadArrayFromFile(string filename)
+		{
+            if (File.Exists(filename))
+            {
+                string[] arr = File.ReadAllLines(filename);
+                row = arr.Length;
+                column = arr[0].Split().Length;
+                array = new int[row, column];
+
+                for (int i = 0; i < row; i++)
+                {
+                    string[] rowArr = arr[i].Split();
+                    for (int j = 0; j < column; j++) array[i, j] = int.Parse(rowArr[j]);
+                }
+            }
+            else
+			{
+                Console.WriteLine("Error load file");
+            }
+        }
+
+        public void WriteArrayFromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                string[] rows = new string[row];
+                string[] columns = new string[column];
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++) columns[j] = array[i, j].ToString();
+                    rows[i] = string.Join(" ", columns);
+                }
+
+                File.WriteAllLines(filename, rows);
+            }
+            else
+            {
+                Console.WriteLine("Error load file");
+            }
+        }
+
+        public void Print()
+		{
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++) Console.Write("{0,4}", array[i, j]);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeWork_lesson4/Test5.DemonstrationLibrary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5.TwoDimensionalArray;
+
+namespace Test5.DemonstrationLibrary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ArrayTwoDimensional array = new ArrayTwoDimensional(5, 6);
+
+			Console.WriteLine("Array (created by constractor with random numbers):");
+			array.Print();
+
+			Console.WriteLine($"Max element: {array.Max}");
+			Console.WriteLine($"Min element: {array.Min}");
+			
+			int sum = array.SumArray();
+			Console.WriteLine($"Sum elements: {sum}");
+			sum = array.SumArray(10);
+			Console.WriteLine($"Sum elements which are more that 10: {sum}");
+
+			int row;
+			int column;
+			array.NumberMaxElement(out row, out column);
+			Console.WriteLine($"Row and column max element: {row} {column}");
+
+			ArrayTwoDimensional array1 = new ArrayTwoDimensional(@"D:\Learning\GeekBrains_C#\Basic_CSharp\Basic_CSharp_Homework\HomeWork_lesson4\Test5.DemonstrationLibrary\read_array.txt");
+			Console.WriteLine("Array (created by constractor with read from file):");
+			array1.Print();
+
+			array.WriteArrayFromFile(@"D:\Learning\GeekBrains_C#\Basic_CSharp\Basic_CSharp_Homework\HomeWork_lesson4\Test5.DemonstrationLibrary\write_array.txt");
+			Console.ReadKey();
+		}
+	}
+}
