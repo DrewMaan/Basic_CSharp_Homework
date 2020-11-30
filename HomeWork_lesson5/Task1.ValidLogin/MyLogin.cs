3bb126f8eb5ff879462d2e5ed5d32744653d47ca
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-30 14:57:45 +0300

    Add task 1 valid login

---
+++ HomeWork_lesson5/Task1.ValidLogin/MyLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.ValidLogin
+{
+	/*
+	 * 
+	 * а) без использования регулярных выражений
+	 * 
+	 */
+	class MyLogin
+	{
+		public string Login { get; set; }
+
+		public void CheckLogin()
+		{
+			if (Login.Length < 2 || Login.Length > 10)
+			{
+				Console.WriteLine("Invalid login!");
+				return;
+			}
+			Char[] loginArray = Login.ToCharArray();
+			if(!Char.IsDigit(loginArray[0]))
+			{
+				for(int i = 0; i < loginArray.Length; i++)
+				{
+					if ((loginArray[i] >= 'a' && loginArray[i] <= 'z') || (loginArray[i] >= 'A' && loginArray[i] <= 'Z') || (loginArray[i] >= '0' && loginArray[i] <= '9'))
+					{
+						continue;
+					}
+					else
+					{
+						Console.WriteLine("Invalid login!");
+						return;
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine("Invalid login!");
+				return;
+			}
+
+			Console.WriteLine("Login OK!");
+		}
+	}
+}
