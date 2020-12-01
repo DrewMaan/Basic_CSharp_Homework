afd2803b5564c53466aaf8452eddf22b68f0eae2
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-12-01 21:05:15 +0300

    Add Task 2 message

---
+++ HomeWork_lesson5/Task2.Message/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Message
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] text;
+
+			ReadFromFile(@"../../mytext.txt", out text);
+
+			if(text != null)
+			{
+				Message.OutPutWordsShorter(7, text);
+				Message.DeleteWordsWhichEndOnSymbol('a', ref text);
+				int lenght = Message.FindTheLongestWord(text);
+				Console.WriteLine($"Length: {lenght}");
+				Console.WriteLine();
+				StringBuilder str = Message.StringFromLongestWords(text);
+
+				Console.WriteLine("The longest words:");
+				Console.WriteLine(str.ToString());
+				Console.WriteLine();
+			}
+
+			Console.ReadKey();
+		}
+
+		static void ReadFromFile(string filename, out string[] message)
+		{
+			if(File.Exists(filename))
+			{
+				message = File.ReadAllLines(filename);
+			}
+			else
+			{
+				Console.WriteLine("Error load file!");
+				message = null;
+			}
+		}
+	}
+}
