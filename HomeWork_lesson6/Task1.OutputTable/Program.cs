892f5c8924a96c9246ebe56e6b98b4aa3ccad787
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-12-05 17:14:00 +0300

    Add solution HomeWork_lesson6 and project Task1.OutputTable

---
+++ HomeWork_lesson6/Task1.OutputTable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.OutputTable
+{		
+	public delegate double Fun(double x, double a);
+	class Program
+	{
+		public static void Table(Fun f, double x, double a, double b)
+		{
+			Console.WriteLine("----- X ----- Y -----");
+			while (x <= b)
+			{
+				Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, f(x, a));
+				x++;
+			}
+			Console.WriteLine("---------------------");
+		}
+
+		public static double MyFunc1(double x, double a)
+		{
+			return a * (x * x);
+		}
+
+		public static double MyFunc2(double x, double a)
+		{
+			return a * Math.Sin(x);
+		}
+
+		static void Main(string[] args)
+		{
+			double a = 5;
+			Console.WriteLine($"Table of func ({a:F2} * x^2):");
+			Table(new Fun(MyFunc1), -2, a, 2);
+			a = 10;
+			Console.WriteLine($"Table of func ({a:F2} * sin(x)):");
+			Table(MyFunc2, -2, a, 2);
+
+			Console.ReadKey();
+		}
+	}
+}
