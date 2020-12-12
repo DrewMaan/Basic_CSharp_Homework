6aea3da9f573bbe35b038963b5cb5d0a67909a8d
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-12-12 18:11:10 +0300

    Add task 2 game guess number

---
+++ Homework_lesson7/Task2.GuessNumber/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.GuessNumber
+{
+	class Game
+	{
+		private int number;
+		private Random random;
+
+		public int Number { get { return number; } }
+		public int Attempts { get; private set; }
+
+		public Game()
+		{
+			random = new Random();
+		}
+
+		public void StartGame()
+		{
+			number = random.Next(1, 100);
+			Attempts = 20;
+		}
+
+		public void StopGame()
+		{
+			number = default;
+			Attempts = default;
+		}
+
+		public bool TryGuess(int n, out string c)
+		{
+			c = "";
+			if (n == number) return true;
+			else
+			{
+				Attempts--;
+				if (n < number) c = "больше";
+				else c = "меньше";
+				return false;
+			}
+		}
+	}
+}
---
+++ Homework_lesson7/Task2.GuessNumber/View.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task2.GuessNumber
+{
+	public partial class View : Form
+	{
+		Presenter presenter;
+		public View()
+		{
+			InitializeComponent();
+
+			presenter = new Presenter();
+
+			Update(false);
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			presenter.Start();
+			Update(true);
+		}
+
+		private void btnEnd_Click(object sender, EventArgs e)
+		{
+			presenter.Stop();
+			Update(false);
+		}
+
+		void Update(bool enable)
+		{
+			btnEnd.Enabled = enable;
+			btnTryGuess.Enabled = enable;
+			txtbxValues.Enabled = enable;
+			if(enable) lblAttempts.Text = "У вас осталось " + presenter.GetAttempts() + " попыток!";
+			else lblAttempts.Text = "У вас не осталось попыток";
+		}
+
+		private void btnTryGuess_Click(object sender, EventArgs e)
+		{
+			bool endgame;
+			string compare = presenter.TryGuess(txtbxValues.Text, out endgame);
+			if (endgame)
+			{
+				Update(false);
+				lblCompare.Text = "";
+			}
+			else
+			{
+				Update(true);
+				lblCompare.Text = "Загаданное число " + compare + " чем " + txtbxValues.Text;
+			}
+			txtbxValues.Text = "";
+		}
+	}
+}
